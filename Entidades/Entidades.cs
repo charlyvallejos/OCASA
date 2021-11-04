417e45f54205af2bdc1f17a36ff548d4a1320c64
charlyvallejos <charlyvallejos22@gmail.com>
2021-11-04 11:47:40 -0300

    Add files via upload

---
+++ Entidades/Entidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocasa.Entidades
+{    
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string RazonSocial { get; set; }
+        public string Direccion { get; set; }
+    }
+    
+}
